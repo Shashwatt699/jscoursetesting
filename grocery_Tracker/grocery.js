6cc403c1ff503f3ed483ae2bfb2ce562c5a4795b
Shashwatt699 <shashwatsaxena4545@gmail.com>
2025-06-01 15:06:15 -0400

    new activity

--- grocery_Tracker/grocery.js
+++ grocery_Tracker/grocery.js
@@ -1,8 +1,8 @@
-let grocery1 = parseFloat(document.getElementById(grocery1).value);
-let grocery2 = parseFloat(document.getElementById(grocery2).value);
-let grocery3 = parseFloat(document.getELementById(grocery3).value);
+const calculateGrocery = () => {
+    let grocery1 = parseFloat(document.getElementById("grocery1").value);
+    let grocery2 = parseFloat(document.getElementById("grocery2").value);
+    let grocery3 = parseFloat(document.getElementById("grocery3").value);
 
-const calculateGrocery = (grocery1 , grocery2, grocery3) => {
     let result = grocery1 + grocery2 + grocery3;
-    document.getElementById("result").innerHTML = `The Total amount is : $result`;
+    document.getElementById("result").innerText = `The Total amount is : ${result}`;
 }
