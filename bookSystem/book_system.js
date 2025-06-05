602a850e3d1fb18aec6f89e749bc7056f3e18d6d
Shashwatt699 <shashwatsaxena4545@gmail.com>
2025-06-05 14:05:38 -0400

    HW Done

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -28,7 +28,8 @@ function showbooks() {
     <p><strong>Book Description</strong> ${book.bookDescription}</p>
     <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
 
-    <button onclick="editbook(${index})">Edit</button>`
+    <button onclick="editbook(${index})">Edit</button>
+    <button onclick="deletebook(${index})">Delete</button> `
    );
    document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -48,4 +49,9 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deletebook(index){
+    books.splice(index, 1);
+    showbooks();
 }
