dd991547c6a362e55d9950a3693429a170e19453
Shashwatt699 <shashwatsaxena4545@gmail.com>
2025-06-05 05:57:35 -0400

    Homeworkd

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,16 +1,18 @@
 const employees = [
     { id:1, name: 'John Doe', age: 30, department: 'IT', 
-    salary: 50000 },
+    salary: 50000, specialization: 'JavaScript' },
     { id: 2, name: 'Alice Smith', age: 28, department: 'HR',
-    salary: 45000 },
+    salary: 45000, specialization: 'JavaScript' },
     { id: 3, name: 'Bob Johnson', age:35, department: 'Finance',
-    salary: 60000},
+    salary: 60000, specialization: 'Java'},
 ];
 
-const displayEmployees = employees.map(
+function displayEmployees(){
+    const totalEmployees = employees.map(
     (employee,index)=>`<p>${employee.id}: ${employee.name}: ${employee.age} - ${employee.department} - $${employee.salary}</p>`).join('');
 
-document.getElementById('employeesDetails').innerHTML = displayEmployees;
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
 
 function calculateTotalSalaries(){
     const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
@@ -32,5 +34,17 @@ function findEmployeeById(employeeId){
         <p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.age} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     }else{
         document.getElementById('employeesDetails').innerHTML = 'no employees has been found with this ID';
+    }
+}
+
+function findEmployeesBySpecialization(){
+    const SpecialEmployee = employees.filter(employee => employee.specialization === 'JavaScript');
+    if(SpecialEmployee.length > 0){
+        const html = SpecialEmployee.map(employee =>
+            `<p>${employee.id} - ${employee.name} : ${employee.age} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`
+        ).join('');
+        document.getElementById('employeesDetails').innerHTML = html;
+    }else{
+        document.getElementById('employeesDetails').innerHTML = 'NO employees found';
     }
 }
